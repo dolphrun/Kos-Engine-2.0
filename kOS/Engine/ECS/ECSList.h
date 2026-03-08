b9a75aff84272b85bbb9d3fc1dd4ec188029f985
Ng Jaz Winn <142562084+jazwinn@users.noreply.github.com>
2026-03-08 23:07:32 +0800

    Update ECSList.h

--- kOS/Engine/ECS/ECSList.h
+++ kOS/Engine/ECS/ECSList.h
@@ -24,7 +24,7 @@ namespace ecs {
 	using ComponentSignature = std::bitset<MAXCOMPONENT>;
 
 	// Max number of entities allowed to be created
-	static EntityID MaxEntity = 2048;
+	static EntityID MaxEntity = 4096;
 
 	enum GAMESTATE {
 		START,
